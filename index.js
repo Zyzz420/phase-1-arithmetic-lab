3b04ef10e0c3ff98120083e8c597131d5e27077b
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-06-22 08:01:34 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+ console.log(multiply);
+
+ let random = 1;
+ Math.random();
+ console.log(random) ;
+
+ let num3 = 18;
+ let num4 = 7;
+ let mod = num3 % num4;
+ console.log(mod);
+
+ let num = (4, 8, 10, 15, 20);
+ let max = Math.max(4, 8, 10, 15, 20)
+ console.log(max);
